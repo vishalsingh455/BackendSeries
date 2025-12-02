d9a8d4407a2a59c90de06cfd8ee16b955aa6ac6e
Vishal Singh <thevishalsingh79@gmail.com>
2025-12-02 20:38:26 +0530

    add subscription model

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,6 +3,22 @@ import {ApiError} from '../utils/ApiError.js'
 import {User} from '../models/user.model.js'
 import {uploadOnCloudinary} from '../utils/cloudinary.js'
 import { ApiResponse } from '../utils/ApiResponse.js'
+import jwt from 'jsonwebtoken'
+
+const generateAccessAndRefreshTokens = async (userId) => {
+  try {
+    const user = await User.findById(userId)
+    const accessToken = user.generateAccessToken()
+    const refreshToken = user.generateRefreshToken()
+
+    user.refreshToken = refreshToken
+    await user.save({ validateBeforeSave:false })
+
+    return {accessToken, refreshToken}
+  } catch (error) {
+    throw new ApiError(500, "Something went wrong while generating refresh and access tokens")
+  }
+}
 
 const registerUser = asyncHandler( async (req, res) => {
   //get user detail from frontend
@@ -16,7 +32,7 @@ const registerUser = asyncHandler( async (req, res) => {
   // return response
 
   const {username, fullName, email, password} = req.body
-  console.log("Email: ", email);
+  //console.log("Email: ", email);
 
   if(
     [email, password, username, fullName].some((field) => field?.trim() === "")
@@ -24,15 +40,25 @@ const registerUser = asyncHandler( async (req, res) => {
     throw new ApiError(400, "All fields are required")
   }
 
-  const existedUser = User.findOne({
+  const existedUser = await User.findOne({
     $or: [{username }, { email }]
   })
   if(existedUser) {
     throw new ApiError(409, "User already exist")
   }
 
-  const avatarLocalPath = req.files?.avatar[0]?.path;
-  const coverImageLocalPath = req.files?.coverImage[0]?.path;
+  //const avatarLocalPath = req.files?.avatar[0]?.path;
+  //const coverImageLocalPath = req.files?.coverImage[0]?.path;
+  let coverImageLocalPath;
+  if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
+    coverImageLocalPath = req.files.coverImage[0].path
+  }
+
+  let avatarLocalPath;
+  if(req.files && Array.isArray(req.files.avatar) && req.files.avatar.length > 0) {
+    avatarLocalPath = req.files.avatar[0].path
+  }
+
   if(!avatarLocalPath) {
     throw new ApiError(400, "Avatar file is required")
   }
@@ -70,4 +96,131 @@ const registerUser = asyncHandler( async (req, res) => {
 
 })
 
-export {registerUser}
+const loginUser = asyncHandler(async (req, res) => {
+  //req body -> data
+  //username or email
+  //find the user
+  //password check
+  //access and refresh token
+  //send cookie
+
+  const {email, username, password} = req.body
+  console.log(email);
+
+  if(!username && !password) {
+    throw new ApiError(400, "username or password is required")
+  }
+
+  const user = await User.findOne({
+    $or: [{ username }, { email }]
+  })
+
+  if(!user) {
+    throw new ApiError(404, "User does not exist")
+  }
+
+  const isPasswordValid = await user.isPasswordCorrect(password);
+  if(!isPasswordValid) {
+    throw new ApiError(401, "Invalid User Credentials")
+  }
+
+  const {accessToken, refreshToken} = await generateAccessAndRefreshTokens(user._id)
+
+  const loggedInUser = await User.findById(user._id).select("-password -refreshToken")
+
+  const options = {
+    httpOnly:true,
+    secure:true
+  }
+
+  return res
+  .status(200)
+  .cookie("accessToken", accessToken, options)
+  .cookie("refreshToken", refreshToken, options)
+  .json(
+    new ApiResponse(
+      200,
+      {
+        user: loggedInUser, accessToken, refreshToken
+      },
+      "User LoggedIn Successfully..."
+    )
+  )
+})
+
+const logoutUser = asyncHandler(async (req, res) => {
+  await User.findByIdAndUpdate(
+    req.user._id,
+    {
+      $set: {
+        refreshToken:undefined
+      }
+    },
+    {
+      new:true
+    }
+  )
+
+  const options = {
+    httpOnly:true,
+    secure:true
+  }
+  return res
+  .status(200)
+  .clearCookie("refreshToken", options)
+  .clearCookie("accessToken", options)
+  .json(new ApiResponse(200, {}, "User Logged Out Successfully..."))
+})
+
+const refreshAccessToken = asyncHandler(async (req, res) => {
+  const incomingRefreshToken = req.cookies.refreshToken || req.body.refreshToken
+  if(!incomingRefreshToken) {
+    throw new ApiError(401, "Unauthorized Access")
+
+  }
+
+  try {
+    const decodedToken = jwt.verify(
+      incomingRefreshToken,
+      process.env.REFRESH_TOKEN_SECRET
+    )
+  
+    const user = await User.findById(decodedToken?._id);
+    if(!user) {
+      throw new ApiError(401, "Invalid refresh token")
+    }
+  
+    if(incomingRefreshToken !== user.refreshToken) {
+      throw new ApiError(401, "Refresh token is expired or used")
+    }
+  
+    const options = {
+      httpOnly:true,
+      secure:true
+    }
+  
+    const {accessToken, newRefreshToken} = await generateAccessAndRefreshTokens(user_id)
+  
+    res
+    .status(200)
+    .cookie("refreshToken", newRefreshToken, options)
+    .cookie("accessToken", accessToken, options)
+    .json(
+      new ApiResponse(
+        200,
+        {
+          accessToken, refreshToken:newRefreshToken
+        },
+        "Access Token Refreshed Successfully"
+      )
+    )
+  } catch (error) {
+    throw new ApiError(401, error?.message || "Invalid refresh token")
+  }
+})
+export {
+  registerUser,
+  loginUser,
+  logoutUser,
+  refreshAccessToken
+}
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -48,18 +48,23 @@ const userSchema = new Schema({
 }, {timestamps:true})
 
 
-userSchema.pre("save", async function (next) {
-  if(!this.isModified("password")) return next();
-  this.password = await bcrypt.hash(this.password, 10);
-  next();
-})
+// userSchema.pre("save", async function (next) {
+//   if(!this.isModified("password")) return next();
+//   this.password = await bcrypt.hash(this.password, 10);
+//   next();
+// })
+
+userSchema.pre("save", async function () {
+    if (!this.isModified("password")) return;
+    this.password = await bcrypt.hash(this.password, 10);
+});
 
 userSchema.methods.isPasswordCorrect = async function (password) {
   return await bcrypt.compare(password, this.password);
 }
 
 userSchema.methods.generateAccessToken = function(){
-  jwt.sign(
+  return jwt.sign(
     {
       _id:this._id,
       email:this.email,
@@ -74,7 +79,7 @@ userSchema.methods.generateAccessToken = function(){
 }
 
 userSchema.methods.generateRefreshToken = function(){
-  jwt.sign(
+  return jwt.sign(
     {
       _id:this._id,
     },
